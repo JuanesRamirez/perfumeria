1dfbca0d5b068f3aa80534d42f02790cb682244c
JuanesRamirez <estebanjuan421@gmail.com>
2024-05-28 14:11:05 -0500

    Cambiosss

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,20 +1,95 @@
-const fs = require('fs').promises;
-const { X } = require('@mui/icons-material');
-const { register } = require('module');
-const path = require('path');
-const { title } = require('process');
+// const fs = require('fs').promises;
+// const { X } = require('@mui/icons-material');
+// const { register } = require('module');
+// const path = require('path');
+// const { title } = require('process');
 
-const userFilePath = path.join(__dirname,'../../src/componentes/usuariosRegistrados.json')
+// const userFilePath = path.join(__dirname,'../../src/componentes/usuariosRegistrados.json')
 
-const controller ={
-    register: async function(req, res){
-        try{
+// const controller ={
+//     register: async function(req, res){
+//         try{
 
-            const userData = await fs.readFile(userFilePath, "utf-8");
-            const users = JSON.parse(userData);
+//             const userData = await fs.readFile(userFilePath, "utf-8");
+//             const users = JSON.parse(userData);
             
+//             const ultimo = users.length;
+//             const usuarioNuevo = {
+//                 id: ultimo + 1,
+//                 identificacion: req.body.identificacion,
+//                 nombres: req.body.nombres,
+//                 apellidos: req.body.apellidos,
+//                 email: req.body.email,
+//                 direccion: req.body.direccion,
+//                 telefono: req.body.telefono,
+//                 fechaNacimiento: req.body.fechaNacimiento,
+//                 password: req.body.password,
+//                 estado: "Activo",
+//                 rol: "Usuario",
+//                 fechaCreacion: new Date()
+//             };
+
+//             for(x of users){
+//             if(x.email ===req.body.email){
+//                 res.status(400).send("El email ya existe");
+//                 return
+//             }
+//         }
+//         users.push(usuarioNuevo);
+        
+//         await fs.writeFile(userFilePath, JSON.stringify(users,null, 4));
+        
+//         res.status(200).send("Exito");
+//         } catch (error){
+//             console.error("Error al procesar el registro:", error);
+//             res.status(500).send("Error interno del servidor");
+//         }
+//     },
+
+//     login: async function (req, res){
+//         try{
+//             const usersData = await fs.readFile(userFilePath, "utf-8");
+//             const users = JSON.parse(usersData);
+
+//             for(x of users){
+//                 if(
+//                     x.email === req.body.email &&
+//                     x.password === req.body.password &&
+//                     x.rol === req.body.rol 
+//                 ){
+//                    return res.json({
+//                     nombres: x.nombres,
+//                     apellidos: x.apellidos,
+//                     email: x.email,
+//                    });
+//                 }
+//             }
+//             res.json({title:"error"});
+//         } catch (Error){
+//             console.error("Error al procesar el registro:", Error);
+//             res.status(500).send("Error interno del servidor");
+//         }
+//     }, 
+// };
+
+// module.exports = controller;
+
+/*codigo JSON registro local
+const fs = require('fs').promises;
+const { password } = require('@mui/icons-material');
+const path = require('path');
+
+const userFilePath = path.join(__dirname, "../../src/componentes/usuariosRegistrados.json");
+
+const controller = {
+    register: async function (req, res) {
+        try {
+            const usersData = await fs.readFile(userFilePath, "utf8");
+            const users = JSON.parse(usersData);
+
             const ultimo = users.length;
             const usuarioNuevo = {
+
                 id: ultimo + 1,
                 identificacion: req.body.identificacion,
                 nombres: req.body.nombres,
@@ -24,52 +99,153 @@ const controller ={
                 telefono: req.body.telefono,
                 fechaNacimiento: req.body.fechaNacimiento,
                 password: req.body.password,
-                estado: "Activo",
-                rol: "Usuario",
-                fechaCreacion: new Date()
+                estado: "activo",
+                rol: req.body.rol,
+                fecha_creacion: new Date(),
+                ciudad: req.body.ciudad,
             };
 
-            for(x of users){
-            if(x.email ===req.body.email){
-                res.status(400).send("El email ya existe");
-                return
+            for (x of users) {
+                if (x.email === req.body.email || x.identificacion === req.body.identificacion) {
+                    res.status(400).send("El email ya existe");
+                    return;
+                }
             }
-        }
-        users.push(usuarioNuevo);
-        
-        await fs.writeFile(userFilePath, JSON.stringify(users,null, 4));
-        
-        res.status(200).send("Exito");
-        } catch (error){
+
+            users.push(usuarioNuevo);
+
+            await fs.writeFile(userFilePath, JSON.stringify(users, null, 4)),
+
+                res.status(200).send("Usuario creado con exito");
+        } catch (error) {
             console.error("Error al procesar el registro:", error);
             res.status(500).send("Error interno del servidor");
         }
     },
-
-    login: async function (req, res){
-        try{
+    login: async function (req, res) {
+        try {
             const usersData = await fs.readFile(userFilePath, "utf-8");
             const users = JSON.parse(usersData);
 
-            for(x of users){
-                if(
-                    x.email === req.body.email &&
-                    x.password === req.body.password &&
-                    x.rol === req.body.rol 
-                ){
-                   return res.json({
-                    nombres: x.nombres,
-                    apellidos: x.apellidos,
-                    email: x.email,
-                   });
+            //const { email, password, rol } = req.body;
+            // if(users.email === req.body.email){
+            //     console.log("son iguales2")
+            // }
+            // const resultado = users.map((index,user) => {
+            //             if(user[index].email === req.body.email && user[index].password === req.body.password){
+            //                 return res.json(user[index])
+            //             }
+            // })
+
+
+            console.log(req.body.email)
+            console.log(req.body.password)
+            console.log(req.body.rol)
+
+
+
+            for (let i = 0; i < users.length; i++) {
+                if (users[i].email === req.body.email && users[i].password === req.body.password && users[i].rol === req.body.rol) {
+                    console.log("encontrado")
+                    return res.json({
+                        nombres: users[i].nombres,
+                        apellidos: users[i].apellidos,
+                        email: users[i].email,
+                    });
                 }
             }
-            res.json({title:"error"});
-        } catch (Error){
-            console.error("Error al procesar el registro:", Error);
-            res.status(500).send("Error interno del servidor");
+
+            // for (x of users) {
+            //     if (x.email === email && x.password === password && x.rol === rol) {
+            //         console.log("Usuario autenticado:", x);
+            //         return res.json({
+            //             nombres: x.nombres,
+            //             apellidos: x.apellidos,
+            //             email: x.email,
+            //         });
+            //     }
+            // }
+
+            //console.log("Credenciales incorrectas para:", { email, rol,password });
+            res.status(401).json({ error: "Credenciales incorrectas" });
+        } catch (error) {
+            console.error("Error al procesar el inicio de sesión:", error);
+            res.status(500).json({ error: "Error interno del servidor" });
         }
-    }, 
-};
+    },
+}
+module.exports = controller;*/
+
+//codigo JSON registro remoto
+
+const express = require("express") //framework
+const app = express()
+const axios = require('axios'); 
+const cors = require("cors"); 
+app.use(cors());
+
+const controller ={
+    register: function(req,res){
+        let config = {
+            method: "GET",
+            maxBodyLength: Infinity,
+            url: 'https://api.jsonbin.io/v3/b/66562705ad19ca34f870ac1d',
+            headers: {
+              'Content-Type': 'application/json',
+              "X-Master-Key": "$2a$10$d.TKppa3f329XgMq4Lx/RuWcgcIZaZoEx9A4WCKKZdPbSXwPRrhze"
+            }
+        };
+        axios(config)
+            .then(result => {
+                let id = result.data.record.length+1
+                    const usuarioNuevo = {
+
+                        id: id,
+                        identificacion: req.body.identificacion,
+                        nombres: req.body.nombres,
+                        apellidos: req.body.apellidos,
+                        email: req.body.email,
+                        direccion: req.body.direccion,
+                        telefono: req.body.telefono,
+                        fechaNacimiento: req.body.fechaNacimiento,
+                        password: req.body.password,
+                        estado: "activo",
+                        rol: req.body.rol,
+                        fecha_creacion: new Date(),
+                        ciudad: req.body.ciudad,
+                    };
+                    if(result.data.record.length === 0){
+                        result.data.record.push(usuarioNuevo);
+                    }else{
+                        for(x of result.data.record){
+                            if(x.email === req.body.email){
+                                res.status(400).send("Usuario ya existe")
+                                return
+                            }
+                        }
+                        result.data.record.push(usuarioNuevo)
+                    }
+
+                    fetch("https://api.jsonbin.io/v3/b/66562705ad19ca34f870ac1d", {
+                        method : "PUT",
+                        headers:{
+                            'Content-Type': 'application/json',
+                            "X-Master-Key": "$2a$10$d.TKppa3f329XgMq4Lx/RuWcgcIZaZoEx9A4WCKKZdPbSXwPRrhze"
+                        },
+                        body: JSON.stringify(result.data.record)
+                    })
+                    .then(response => {
+                        if (response.status === 200) {
+                          res.status(200).send('ok')
+                          return
+                        }
+                        else {
+                          res.status(400).send("No Ok")
+                          return
+                        }
+                    })
+            })
+    }
+}
 
 module.exports = controller;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const axios = require('axios');
 
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(express.json());
@@ -9,12 +10,25 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/",(req,res) =>{
-    res.send("Saludando desde el backend")
+    // res.send("Saludando desde el backend")
+    let config = {
+        method: "GET",
+        maxBodyLength: Infinity,
+        url: 'https://api.jsonbin.io/v3/b/66562705ad19ca34f870ac1d',
+        headers: {
+          'Content-Type': 'application/json',
+          "X-Master-Key": "$2a$10$d.TKppa3f329XgMq4Lx/RuWcgcIZaZoEx9A4WCKKZdPbSXwPRrhze"
+        }
+    };
+    axios(config)
+        .then(result => {
+            res.send(result.data.record)
+        })
 })
 
 const user = require("./controller/userController");
 app.use("/registro-usuario",user.register);
-app.use("/login",user.login);
+//app.use("/login",user.login);
 
 const PORT = 3001
 app.listen(PORT,()=>{
